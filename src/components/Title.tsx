02987a8bacec11df198ba47313f78d2437816518
july2m3 <july2m3@gmail.com>
2020-06-15 17:05:50 -0500

    Update UI

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,11 +3,10 @@ import React from 'react';
 const Title = (props: any) => {
   const { today } = props;
   return (
-    <header className="title">
-      <h1>The daily Grind</h1>
-      <h3 className="red">
+    <header className='title'>
+      <h1>☕The Daily Grind☕</h1>
+      <h3 className='red'>
         {today}
-        <i className="fas fa-calendar-times" />
       </h3>
     </header>
   );
